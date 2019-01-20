cb27a2422dc5d7169c7d74552f935e4fd92affc6
shadow-22 <el13302@central.ntua.gr>
2019-01-20 18:56:29 +0200

    lala

--- prolog.pl
+++ prolog.pl
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 :- dynamic client/8.
 :- dynamic node/3.
 :- dynamic belongsTo/2.
@@ -14,3 +15,24 @@ canMoveFromTo(NodeIdA, NodeIdB) :-
     node(_, _, NodeIdA, LineId, CounterA),
     node(_, _, NodeIdB, LineId, CounterB),
     direction(LineId, CounterA, CounterB).
+=======
+:- dynamic client/8.
+:- dynamic node/3.
+:- dynamic belongsTo/2.
+
+trafficBottleneck(LineId, CongestionLevel) :-
+    CallTime(Time),
+    RoadTraffic(LineId, BeginTime, EndTime, Congestion),
+    BeginTime =< Time,
+    EndTime >= Time,
+    
+
+calculateCost(Ax, Ay, Bx, By, Value) :-
+    node(Ax, Ay, _, LineId, _, _),
+    node(Bx, By, _, LineId, _, _),
+    trafficBottleneck(LineId, CongestionLevel),
+    Value is CongestionLevel.
+
+
+
+>>>>>>> Stashed changes
