e4d05b5c201e54844a6899ffda4991a5e7667111
ntziris <ntziris@gmail.com>
2019-01-20 18:44:20 +0200

    canMoveFromTo

--- src/gr/ntua/ece/NodeWithPriority.java
+++ src/gr/ntua/ece/NodeWithPriority.java
@@ -22,4 +22,8 @@ public class NodeWithPriority implements Comparator<NodeWithPriority>
     public Node getNode() {
         return x;
     }
+
+    public double getCost() {
+        return cost;
+    }
 }
