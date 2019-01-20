d2a0bb1b83151d083477690657a4972743c34b79
ntziris <ntziris@gmail.com>
2019-01-20 19:11:06 +0200

    canMoveFromTo

--- prolog.pl
+++ prolog.pl
@@ -8,11 +8,22 @@ trafficBottleneck(LineId, CongestionLevel) :-
     BeginTime =< Time,
     EndTime >= Time.
 
-
 calculateCost(Ax, Ay, Bx, By, Value) :-
     node(Ax, Ay, _, LineId, _, _),
     node(Bx, By, _, LineId, _, _),
     trafficBottleneck(LineId, CongestionLevel).
+
+
+/* direction and canMoveFromTo, to figure out if its allowed to move from one node to another */
+direction(LineId, CounterA, CounterB) :-
+    line(LineId, _, _, OneWay, _, _, _, no, no, no, no, no, _, _, _, no, _, _),
+    OneWay = yes,
+    CounterA < CounterB.
+
+direction(LineId, CounterA, CounterB) :-
+    line(LineId, _, _, OneWay, _, _, _, no, no, no, no, no, _, _, _, no, _, _),
+    OneWay = -1,
+    CounterA > CounterB.
 
 canMoveFromTo(NodeIdA, NodeIdB) :-
     node(_, _, NodeIdA, LineId, CounterA),
